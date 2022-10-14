797a0a7909ae13b915e9962d033563c417d3110c
Byron Rau <byronrau@gmail.com>
2022-10-14 14:45:08 -0700

    Add docstring and removed prints

--- project1.py
+++ project1.py
@@ -1,6 +1,12 @@
+"""Project 1 pi-athlon
+
+This project is intended to have the pi-athlon follow a line and self drive
+itself along a track.
+
+"""
+
 #!/usr/bin/python
 
-import time
 import RPi.GPIO as GPIO
 import Robot
 
@@ -16,31 +22,24 @@ RIGHT_TRIM  = 0
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 
-
 while True:
     RIGHT_SENSOR = GPIO.input(13)
     LEFT_SENSOR = GPIO.input(16)
 
     if (RIGHT_SENSOR == 1) and (LEFT_SENSOR == 0):
-        # print("right sensor detected")
         robot.right(40)
         continue
-    
+
     if (LEFT_SENSOR == 1) and (RIGHT_SENSOR == 0):
-        # print("left sensort detected")
         robot.left(40)
         continue
-    
+
     if (RIGHT_SENSOR == 1) and (LEFT_SENSOR == 1):
-        # robot.backward(100, 0.2)
         robot.forward(90)
         continue
 
 
     if (RIGHT_SENSOR == 0) and (LEFT_SENSOR == 0):
-        # robot.forward(40, 0.2)
         robot.backward(40)
         continue
-
-
-    
+ 
