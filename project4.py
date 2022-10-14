b792f491c66518bbb7c5a6faa1087265ba0e5844
Byron Rau <byronrau@gmail.com>
2022-10-14 15:50:31 -0700

    Add docstring and removed prints for project4.py

--- project4.py
+++ project4.py
@@ -1,5 +1,8 @@
-import time
-# robot
+"""Project 4 pi-athlon
+
+This project is intended to have the pi-athlon use a UI to move
+
+"""
 import RPi.GPIO as GPIO
 import Robot
 # flask
@@ -9,11 +12,11 @@ from flask import jsonify
 from flask import request
 
 # init pins
-buzzer_pin = 15
-rgb_pins = {"pin_R": 35, "pin_G": 38, "pin_B": 40}
-red = 0x00FFFF
-green = 0xFF00FF
-blue = 0xFFFF00
+BUZZER_PIN = 15
+RGB_PINS = {"pin_R": 35, "pin_G": 38, "pin_B": 40}
+RED = 0x00FFFF
+GREEN = 0xFF00FF
+BLUE = 0xFFFF00
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -24,16 +27,16 @@ RIGHT_TRIM = 0
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 # rgb
-for pin in rgb_pins:
+for _, pin in RGB_PINS.items():
     # Set pins' mode is output
-    GPIO.setup(rgb_pins[pin], GPIO.OUT)
+    GPIO.setup(RGB_PINS[pin], GPIO.OUT)
     # Set pins to high(+3.3V) to off led
-    GPIO.output(rgb_pins[pin], GPIO.HIGH)
+    GPIO.output(RGB_PINS[pin], GPIO.HIGH)
 
 # set Frequece to 2KHz
-p_R = GPIO.PWM(rgb_pins['pin_R'], 2000)
-p_G = GPIO.PWM(rgb_pins['pin_G'], 2000)
-p_B = GPIO.PWM(rgb_pins['pin_B'], 2000)
+p_R = GPIO.PWM(RGB_PINS['pin_R'], 2000)
+p_G = GPIO.PWM(RGB_PINS['pin_G'], 2000)
+p_B = GPIO.PWM(RGB_PINS['pin_B'], 2000)
 
 # Initial duty Cycle = 0(leds off)
 p_R.start(0)
@@ -42,35 +45,52 @@ p_B.start(0)
 
 
 def setup():
+    """Setup buzzer pins"""
     # buzzer
-    GPIO.setup(buzzer_pin, GPIO.OUT)
+    GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
 
-def map_rgb(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+def map_rgb(x_val, in_min, in_max, out_min, out_max):
+    """Sets the RGB values for ligh
+
+    Args:
+        x (int): x value
+        in_min (int): input min
+        in_max (int): input max
+        out_min (int): output min
+        out_max (int): output max
+    """
+    return (x_val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 def set_color(color_str):
-    R_val = (color_str & 0xff0000) >> 16
-    G_val = (color_str & 0x00ff00) >> 8
-    B_val = (color_str & 0x0000ff) >> 0
+    """Sets color
 
-    R_val = map_rgb(R_val, 0, 255, 0, 100)
-    G_val = map_rgb(G_val, 0, 255, 0, 100)
-    B_val = map_rgb(B_val, 0, 255, 0, 100)
+    Args:
+        color_str (str): name of color
+    """
+    r_val = (color_str & 0xff0000) >> 16
+    g_val = (color_str & 0x00ff00) >> 8
+    b_val = (color_str & 0x0000ff) >> 0
+
+    r_val = map_rgb(r_val, 0, 255, 0, 100)
+    g_val = map_rgb(g_val, 0, 255, 0, 100)
+    b_val = map_rgb(b_val, 0, 255, 0, 100)
 
     # Change duty cycle
-    p_R.ChangeDutyCycle(R_val)
-    p_G.ChangeDutyCycle(G_val)
-    p_B.ChangeDutyCycle(B_val)
+    p_R.ChangeDutyCycle(r_val)
+    p_G.ChangeDutyCycle(g_val)
+    p_B.ChangeDutyCycle(b_val)
 
 
 def play_buzzer(time_in_secs):
+    """Plays Buzzer"""
     # todo
     pass
 
 
 def move_robot(direction, time_in_secs=2, speed=50):
+    """Moves Robot"""
     # toto
     pass
 
@@ -83,19 +103,21 @@ setup()
 
 @app.route("/")
 def home():
+    """Returns Home"""
     return render_template("index.html")
 
 
 @app.route("/led/<color>")
 def app_set_color(color):
+    """Sets Color"""
     print("color", color)
     # set the LED of the robot for specified color
     if color == "red":
-        set_color(red)
+        set_color(RED)
     if color == "green":
-        set_color(green)
+        set_color(GREEN)
     if color == "blue":
-        set_color(blue)
+        set_color(BLUE)
 
     # return message to client
     return jsonify({"led": color})
@@ -103,6 +125,7 @@ def app_set_color(color):
 
 @app.route("/direction/<direction>")
 def app_move_robot(direction):
+    """Moves robot"""
     speed = int(request.args.get("speed"))
     duration = int(request.args.get("duration"))
     print("speed", speed)
@@ -120,6 +143,7 @@ def app_move_robot(direction):
 
 @app.route("/buzzer")
 def app_play_buzzer():
+    """Plays Buzzer"""
     print("buzzer")
     # activate the robot buzzer
     play_buzzer(5)
